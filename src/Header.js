136fdfa2417d22a3f97e04829ea7c611e6a35fd4
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-12-09 12:00:03 +0000

    Add a responsive menu icon on smaller screens

--- src/Header.js
+++ src/Header.js
@@ -1,28 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './robinhood.svg';
+import CloseIcon from '@material-ui/icons/Close';
 import './Header.css';
 function Header() {
+  const addClassName = (e) => {
+    const element = e.target;
+    if (element.classList.contains('open')) {
+      element.classList.remove('open');
+      setShowMenu(false);
+    } else {
+      element.classList.add('open');
+      setShowMenu(true);
+    }
+  };
+
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <div className="header__wrapper">
-      {/* logo */}
-      <div className="header__logo">
-        <img src={logo} alt="logo" width={25} />
-        {/* search bar */}
-        {/* menu items */}
+    <>
+      <div className="header__navigation" onClick={addClassName}>
+        <div className="header__icon"></div>
       </div>
-      <div className="header__search">
-        <div className="header__searchContainer">
-          <input placeholder="Search" type="text" />
+      <div className="header__wrapper">
+        <div className="header__logo">
+          <img src={logo} alt="logo" width={25} />
+        </div>
+        <div className="header__search">
+          <div className="header__searchContainer">
+            <input placeholder="Search" type="text" />
+          </div>
+        </div>
+
+        <div className="header__menuItems">
+          <a href="#">Free Stocks</a>
+          <a href="#">Porfolio</a>
+          <a href="#">Cash</a>
+          <a href="#">Messages</a>
+          <a href="#">Account</a>
         </div>
       </div>
-      <div className="header__menuItems">
-        <a href="#">Free Stocks</a>
-        <a href="#">Porfolio</a>
-        <a href="#">Cash</a>
-        <a href="#">Messages</a>
-        <a href="#">Account</a>
+      <div className={`mobile-menu overlay ${showMenu ? 'open' : 'close'}`}>
+        <div className="header__menuItems">
+          <a href="#">Free Stocks</a>
+          <a href="#">Porfolio</a>
+          <a href="#">Cash</a>
+          <a href="#">Messages</a>
+          <a href="#">Account</a>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
